4b61d43c8c11cc05a4be2077ad3b1070d1764ea6
Lydia Vega <lyvegama@gmail.com>
2023-04-13 20:20:02 +0200

    entregado

--- client/src/components/Info/Configuracion.js
+++ client/src/components/Info/Configuracion.js
@@ -6,9 +6,9 @@ const Configuracion = () => {
         <div className='configuracion'>
             <p>MESES ABIERTOS A DECLARACIONES</p>
             <div className='spanConfigurar'>
-            <span class="material-symbols-outlined">settings</span>
+                <span class="material-symbols-outlined">settings</span>
                 <p>Configurar</p>
-        </div>
+            </div>
         </div>
     )
 }
